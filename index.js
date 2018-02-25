54073f276176145ceb119216ee28bbb38bd87794
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-02-25 23:05:02 +0530

    web framework

---
+++ index.js
@@ -0,0 +1,12 @@
+// express initializes app to be a function handler that you can supply to an http server
+var app = require('express')();
+var http = require('http').Server(app);
+
+// we define a route handler / that get called when we hit our website home.
+app.get('/', function(request, response){
+  resonse.sendFile('<h1>hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
